7ffbdd6cc519f4863f3c6f6218fe4d8d3b652d04
Osepyan Alexander <a.e.osepyan@gmail.com>
2025-12-21 18:44:15 +0300

    Update nvim downbload link

--- profile.sh
+++ profile.sh
@@ -26,7 +26,7 @@ $HOME/.local/bin/poetry config virtualenvs.in-project true
 
 # ----Install neovim----
 if [[ ! -L /usr/local/bin/nvim ]]; then
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 sudo ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
